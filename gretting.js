8023390054603ad993930532d08fbfd18a06f522
dejoro <ozzymen@naver.com>
2019-01-29 17:01:04 +0900

    추가

--- gretting.js
+++ gretting.js
@@ -1,7 +1,13 @@
-const
+const form = document.querySelector(".js-form"),
+   input = form.querySelector("input");
+
+function loadName() {
+
+}
+
 
 function init() {
-
+   loadName();
 }
 
 init();
