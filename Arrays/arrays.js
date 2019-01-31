86ee2cf0deedcc18e7cdf71f765c802238b17cde
dejoro <ozzymen@naver.com>
2019-01-31 10:10:54 +0900

    추가와수정

---
+++ Arrays/arrays.js
@@ -0,0 +1,7 @@
+var shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
+//shopping;
+
+var sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 13];
+for (var i = 0; i < sequence.length; i++) {
+   console.log(sequence[i]);
+}
